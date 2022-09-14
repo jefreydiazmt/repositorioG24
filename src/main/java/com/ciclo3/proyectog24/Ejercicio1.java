0127ea65be2bb6199d57140bcab75da85f1b3712
JEFREY DIAZ <jefrey.diaz.mt@usa.edu.co>
2022-09-14 18:29:14 -0500

    first commit

---
+++ src/main/java/com/ciclo3/proyectog24/Ejercicio1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ciclo3.proyectog24;
+
+/**
+ *
+ * @author jefrey
+ */
+public class Ejercicio1 {
+    public void metodo(){
+        System.out.print("logicametodo");
+    }
+    
+}
